088cf9e41366301e7b01a7902b1806b54c9e70f2
yusufie <yusufpirejman@gmail.com>
2025-11-01 14:03:44 +0300

    feat: add MongoDB connection and configuration with NestJS modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set global prefix
+  app.setGlobalPrefix('api');
+
+  // Configure CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://mvp.aiseoptimizer.com'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin'],
+    optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
